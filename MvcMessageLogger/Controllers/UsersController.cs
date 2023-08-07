2dcb3f748c4e77bdffc78d8ec0c46977e5193b3d
abukhmirov <alexbukhmirov@gmail.com>
2023-08-07 11:33:39 -0600

    Added Customer Controller and Index

---
+++ MvcMessageLogger/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMessageLogger.DataAccess;
+using MvcMessageLogger.Models;
+
+namespace MvcMessageLogger.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly MvcMessageLoggerContext _context;
+
+        public UsersController(MvcMessageLoggerContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var users = _context.Users;
+           
+            return View(users);
+        }
+    }
+}
