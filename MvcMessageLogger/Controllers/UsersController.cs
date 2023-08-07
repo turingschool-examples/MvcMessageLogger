cb304874dca5c2b16f3e5098aae656383962e594
iworsham <15iworsham@gmail.com>
2023-08-07 17:45:54 -0400

    -tests,controllers,views,new,index,create

---
+++ MvcMessageLogger/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMessageLogger.DataAccess;
+using MvcMessageLogger.Models;
+
+namespace MvcMessageLogger.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly MvcMessageLoggerContext _context;
+
+        public UsersController(MvcMessageLoggerContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var users = _context.Users.ToList();
+            return View(users);
+        }
+        public IActionResult New()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+           // var newUserId = user.Id;
+
+            return RedirectToAction("index");
+
+        }
+    }
+}
