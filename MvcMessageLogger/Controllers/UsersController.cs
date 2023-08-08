fc6aca4a2f5f5d0b33e4a376983c36a9ebff51da
abukhmirov <alexbukhmirov@gmail.com>
2023-08-08 13:12:40 -0600

    Finished

--- MvcMessageLogger/Controllers/UsersController.cs
+++ MvcMessageLogger/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MvcMessageLogger.DataAccess;
 using MvcMessageLogger.Models;
 
@@ -31,6 +32,33 @@ namespace MvcMessageLogger.Controllers
             
 
             return RedirectToAction("Index");
+        }
+
+        [Route("/users/details/{id:int}")]
+        public IActionResult Details(int Id)
+        {
+            var user = _context.Users.Include(u => u.Messages).FirstOrDefault(u => u.Id == Id);
+            
+            return View(user);
+        }
+
+        [Route("/users/{userId:int}/messages/new")]
+        public IActionResult NewMessage(int userId)
+        {
+            var user = _context.Users.Where(u => u.Id == userId).Include(u => u.Messages).First();
+            return View(user);
+        }
+
+        [HttpPost]
+        [Route("/users/{userId:int}/messages")]
+        public IActionResult CreateMessage(Message message, int userId)
+        {
+            var user = _context.Users.Where(u => u.Id == userId).Include(u => u.Messages).First();
+            
+            user.Messages.Add(message);
+            _context.SaveChanges();
+
+            return Redirect($"/users/details/{user.Id}");
         }
     }
 }
--- MvcMessageLoggerTests/UsersControllerTests.cs
+++ MvcMessageLoggerTests/UsersControllerTests.cs
@@ -28,7 +28,7 @@ namespace MvcMessageLoggerTests
             return context;
         }
         [Fact]
-        public async Task Index_ShowsItems()
+        public async Task Index_ShowsUsers()
         {
             var context = GetDbContext();
 
@@ -44,7 +44,7 @@ namespace MvcMessageLoggerTests
 
             response.EnsureSuccessStatusCode();
             // usernames only because that is what the website is showing on the index, name will be shown in Details
-            Assert.Contains("Doe", html); 
+            Assert.Contains("Doe", html);
             Assert.Contains("Don", html);
 
 
@@ -68,7 +68,7 @@ namespace MvcMessageLoggerTests
         }
 
         [Fact]
-        public async Task Create_AddsItemToDB()
+        public async Task Create_AddsUserToDB()
         {
             var client = _factory.CreateClient();
 
@@ -84,6 +84,73 @@ namespace MvcMessageLoggerTests
             response.EnsureSuccessStatusCode();
 
             Assert.Contains("Doe", html);
+        }
+
+        [Fact]
+        public async Task Details_ShowsAllMessagesForUser()
+        {
+            var context = GetDbContext();
+            var client = _factory.CreateClient();
+
+            var user1 = new User { Name = "John", Username = "Doe" };
+
+            var message = new Message { Content = "hello", CreatedAt = new DateTime(2000, 2, 1).ToUniversalTime() };
+
+            user1.Messages.Add(message);
+            context.Users.Add(user1);
+            context.SaveChanges();
+
+            var response = await client.GetAsync("/users/details/1");
+            response.EnsureSuccessStatusCode();
+
+            var html = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("Doe", html);
+            Assert.Contains("hello", html);
+
+        }
+
+        [Fact]
+        public async Task NewMessage_ShowsForm()
+        {
+            var context = GetDbContext();
+            var client = _factory.CreateClient();
+
+            var user1 = new User { Name = "John", Username = "Doe" };
+
+            var message = new Message { Content = "hello", CreatedAt = new DateTime(2000, 2, 1).ToUniversalTime() };
+
+            user1.Messages.Add(message);
+            context.Users.Add(user1);
+            context.SaveChanges();
+
+            var response = await client.GetAsync("/users/1/messages/new");
+            response.EnsureSuccessStatusCode();
+
+            var html = await response.Content.ReadAsStringAsync();
+
+        }
+
+        [Fact]
+        public async Task CreateMessage_AddsMessageToDB()
+        {
+            var context = GetDbContext();
+            var client = _factory.CreateClient();
+            var user = new User { Name = "John", Username = "Doe" };
+            context.Users.Add(user);
+            context.SaveChanges();
+
+            var formData = new Dictionary<string, string>
+            {
+                {"Content","Hello" }
+            };
+
+
+            var response = await client.PostAsync($"/users/{user.Id}/messages", new FormUrlEncodedContent(formData));
+  
+            var html = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("Hello", html);
         }
     }
 }
